b23dc7ab9f14cfa01577034719b471269087347b
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-11 23:40:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+import "./index.css";
+function App() {
+  return (
+    <div className="bg-white">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
